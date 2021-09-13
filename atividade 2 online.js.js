a1250297d50fbba1547874f7ccbdb005ca27a076
fabiola <fabiola.goms770@gmail.com>
2021-09-13 15:46:32 -0300

    cadastro do evento

---
+++ atividade 2 online.js.js
@@ -0,0 +1,28 @@
+var data
+var idade
+var quantidadedeparticipantes
+
+data="15/10";
+idade=18;
+quantidadedeparticipantes=100;
+
+if (data <="15/10"){
+    console.log("cadastrar");
+
+}else (data >="15/10");{
+console.log("não permitido,data inválida");
+
+};
+if (idade >=18){
+    console.log("cadastrar");
+}else if (idade <18);{
+
+    console.log("não permitido, idade inválida");
+}
+if(quantidadedeparticipantes<100);{
+    console.log("realizar cadastro");
+}
+(quantidadedeparticipantes>=100);{
+    console.log("não permitido,excedido limite de cadastros");
+
+}
